29f49054ddbd3aa3be183fd56cd618f7e6adfd69
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-16 17:02:32 -0500

    add model Category add connection string add microsoft entity framework

---
+++ EcommerceProject/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceProject.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
