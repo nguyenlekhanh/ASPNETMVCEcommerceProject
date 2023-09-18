463a58cf2e801a021da76ed58a0562c102f9efc1
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-18 16:00:17 -0500

    Apply other field to register page like name, city,..

--- EcommerceProject/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ EcommerceProject/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -108,6 +108,15 @@ namespace EcommerceProject.Areas.Identity.Pages.Account
             public string? Role { get; set; }
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+            #nullable enable
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
         }
 
 
@@ -144,6 +153,14 @@ namespace EcommerceProject.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.StreetAddress = Input.StreetAddress;
+                user.City = Input.City;
+                user.Name = Input.Name;
+                user.State = Input.State;
+                user.PostalCode = Input.PostalCode;
+                user.PhoneNumber = Input.PhoneNumber;
+
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
