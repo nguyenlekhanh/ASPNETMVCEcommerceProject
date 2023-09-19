bc99846ac72a75e4906412340d1632116c3e6373
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-19 17:26:14 -0500

    implement orderHeader and orderDetail repository

---
+++ EcommerceProject.DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,25 @@
+﻿using EcommerceProject.DataAccess.Data;
+using EcommerceProject.DataAccess.Repository.IRepository;
+using EcommerceProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceProject.DataAccess.Repository
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderDetailRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(OrderDetail obj)
+        {
+            _db.OrderDetails.Update(obj);
+        }
+    }
+}
---
+++ EcommerceProject.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,27 @@
+﻿using EcommerceProject.DataAccess.Data;
+using EcommerceProject.DataAccess.Repository.IRepository;
+using EcommerceProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceProject.DataAccess.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderHeaderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+
+        public void Update(OrderHeader obj)
+        {
+            _db.OrderHeaders.Update(obj);
+        }
+    }
+}
