d2e6d2f6ec6b8cba55d89ef599a31078d4e91f49
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-20 13:34:05 -0500

    add authorization for Admin area

--- EcommerceProject/Areas/Admin/Controllers/CategoryController.cs
+++ EcommerceProject/Areas/Admin/Controllers/CategoryController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Authorization;
 namespace EcommerceProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    //[Authorize(Roles = SD.Role_Admin)]
+    [Authorize(Roles = SD.Role_Admin)]
     public class CategoryController : Controller
     {
         //private readonly ApplicationDbContext _db;
