01e4c6e8451bfa63cd9acabde27bc192fec1a5c2
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-20 11:30:44 -0500

    Apply Order List page

--- EcommerceProject.DataAccess/Repository/OrderHeaderRepository.cs
+++ EcommerceProject.DataAccess/Repository/OrderHeaderRepository.cs
@@ -52,4 +52,4 @@ namespace EcommerceProject.DataAccess.Repository
         }
     }
 }
-}
+
---
+++ EcommerceProject/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,60 @@
+﻿using EcommerceProject.DataAccess.Repository.IRepository;
+using EcommerceProject.Models;
+using EcommerceProjectUtility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceProject.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class OrderController : Controller
+    {
+
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll(string status)
+        {
+            IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+
+            }
+
+
+            return Json(new { data = objOrderHeaders });
+        }
+
+
+        #endregion
+    }
+}
