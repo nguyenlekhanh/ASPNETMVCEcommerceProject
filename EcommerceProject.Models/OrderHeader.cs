a499978b4781846f8362080209a0d2afdf6c91c7
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-20 11:08:14 -0500

    Add Helper Methods in Order Header Repository

--- EcommerceProject.Models/OrderHeader.cs
+++ EcommerceProject.Models/OrderHeader.cs
@@ -31,6 +31,8 @@ namespace EcommerceProject.Models
         public DateTime PaymentDate { get; set; }
         public DateTime PaymentDueDate { get; set; }
 
+        public string? SessionId { get; set; }
+
         public string? PaymentIntentId { get; set; }
 
         [Required]
